a3a58cfde8e0f4a8cc58ee23283a9d41bcd218fa
Kristian Kjeldsen <jg@renegade.se>
2022-11-25 11:37:37 +0100

    almost working

--- hello-world-oldschool/Program.cs
+++ hello-world-oldschool/Program.cs
@@ -6,6 +6,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to my excellent app!");
+        DumboOctopus();
         Console.WriteLine("Please input your username: ");
         string? userName;
 
@@ -15,12 +16,12 @@ class Program
 
         bool mainMenu = true;
         while (mainMenu)
-        { 
-        Console.WriteLine("Please select one of the following");
-        Console.WriteLine("1. Chessboard");
-        Console.WriteLine("2. Dumbo Octopus");
-        Console.WriteLine("E. Exit");
-        Console.Write("----> ");
+        {
+            Console.WriteLine("Please select one of the following");
+            Console.WriteLine("1. Chessboard");
+            Console.WriteLine("2. Dumbo Octopus");
+            Console.WriteLine("E. Exit");
+            Console.Write("----> ");
             string? choice = Console.ReadLine().ToUpper();
             switch (choice)
             {
@@ -52,6 +53,20 @@ class Program
     static void DumboOctopus()
     {
         Console.WriteLine("Welcome to Dumbo Octopus");
+        String input = File.ReadAllText(@"data.txt");
+        int[,] octopus = new int[10, 10];
+        int j = 0;
+
+        foreach (var row in input.Split('\n'))
+        {
+            var intArray = row.Select(c => c - '0').ToArray();
+            for (int i = 0; i < intArray.Count(); i++)
+            {
+                octopus[j, i] = intArray[i];
+            }
+            j++;
+        }
+        /*
         int[,] octopus = new int[5, 5]{
                                 {1, 1, 1, 1, 1},
                                 {1, 9, 9, 9, 1},
@@ -59,9 +74,7 @@ class Program
                                 {1, 9, 9, 9, 1},
                                 {1, 1, 1, 1, 1}
                             };
-        
-
-        
+*/
         printBoard(octopus);
         /*
          * First, the energy level of each octopus increases by 1.
@@ -77,126 +90,184 @@ class Program
             if (!correctInput)
             {
                 Console.WriteLine("Please enter a valid number.");
-            } else
+            }
+            else
             {
                 getInput = false;
             }
-            
+
         }
         Console.WriteLine("You entered " + value + " steps.");
+        Console.WriteLine("===================================");
+        bool keepScanning = true;
+        int totalFlashes = 0;
+        //var flashed;
         for (int step = 0; step <= value; step++)
-        { 
+        {
+            int flashedThisRound = 0;
+            int flashedPreviousRound = 0;
             Console.WriteLine();
             Console.WriteLine("Step number: " + step);
             octopus = increaseEnergy(octopus);
             var flashed = detectFlashed(octopus);
+            if (flashed.Count() == 0)
+            {
+                System.Console.WriteLine("No flashes this step");
+                continue;
+            }
+            flashedThisRound = flashed.Count();
+            while (keepScanning)
+            {
+                flashedPreviousRound = flashedThisRound;
+                octopus = resetFlashed(octopus, flashed);
+                flashedThisRound = 0;
+                foreach (var coord in flashed)
+                {
+                    var adjacent = findAdjacent(octopus, coord);
+                    foreach (var adjacentCoord in adjacent)
+                    {
+                        octopus[adjacentCoord.Item1, adjacentCoord.Item2] += 1;
+                    }
+                }
+                flashed = detectFlashed(octopus);
+                flashedThisRound += flashed.Count();
+                System.Console.WriteLine("Round:");
+                printBoard(octopus);
+                Console.ReadLine();
+                if (flashedThisRound == flashedPreviousRound)
+                    System.Console.WriteLine("No more flashes!");
+                keepScanning = false;
+
+            }
             octopus = resetFlashed(octopus, flashed);
             printBoard(octopus);
-            /*
-            foreach (var tuple in flashed)
-            {
-                Console.WriteLine("{0} - {1}", tuple.Item1, tuple.Item2);
-            }
-            */
-            Console.WriteLine();
+            totalFlashes += flashedThisRound;
+            System.Console.WriteLine("Flashed this round: " + flashedThisRound);
+            System.Console.WriteLine("Total flashes: " + totalFlashes);
+            Console.ReadLine();
         }
-
-        static int[,] resetFlashed(int[,] arr, List<Tuple<int, int>> flashedThisStep)
+        /*
+        foreach (var tuple in flashed)
         {
-            int rows = arr.GetLength(0);
-            int cols = arr.GetLength(1);
-
-            int[,] newOctopus = new int[rows, cols];
-
-            for (int row = 0; row < rows; row++)
-            {
-                for (int col = 0; col < cols; col++)
-                {
-                    Tuple<int, int> current = new Tuple<int, int>(row, col);
-                    if (flashedThisStep.Contains(current))
-                    {
-                        //Console.WriteLine("Holy crap, this one flashed!");
-                        newOctopus[row, col] = 0;
-                    }
-                    else
-                    {
-                        newOctopus[row, col] = arr[row, col];
-                    }
-                    
-                }
-
-            }
-            return newOctopus;
+            Console.WriteLine("{0} - {1}", tuple.Item1, tuple.Item2);
         }
-        static int[,] increaseEnergy(int[,] arr)
-        {
-            int rows = arr.GetLength(0);
-            int cols = arr.GetLength(1);
-
-            int[,] newOctopus = new int[rows, cols];
-
-            for (int row = 0; row < rows; row++)
-            {
-                for (int col = 0; col < cols; col++)
-                {
-                    newOctopus[row, col] = arr[row, col] + 1;
-                }
-
-            }
-            //printBoard(newOctopus);
-            return newOctopus;
-        }
-
-        static List<Tuple<int, int>> detectFlashed(int[,] arr)
-        {
-            int rows = arr.GetLength(0);
-            int cols = arr.GetLength(1);
-
-            int[,] newOctopus = new int[rows, cols];
-            var flashedList = new List<Tuple<int, int>>();
-
-            for (int row = 0; row < rows; row++)
-            {
-                for (int col = 0; col < cols; col++)
-                {
-                    if (arr[row, col] > 9)
-                    {
-                        // Flash!
-                        flashedList.Add(new Tuple<int, int>(row, col));
-                    }
-                }
-            }
-            return flashedList;
-
-        }
-
-        static void findAdjacent()
-        {
-            // for a 
-        }
-        static void printBoard(int[,] arr)
-        {
-            int rows = arr.GetLength(0);
-            int cols = arr.GetLength(1);
-            Console.WriteLine("rows: " + rows);
-            Console.WriteLine("cols: " + cols);
-            for (int row = 0; row < rows; row++)
-            {
-                for (int col = 0; col < cols; col++)
-                {
-                    Console.Write(arr[row, col] + " ");
-                }
-                Console.WriteLine();
-            }
-        }
+        */
+        Console.WriteLine();
     }
 
-        static void ChessBoard()
+    static int[,] resetFlashed(int[,] arr, List<Tuple<int, int>> flashedThisStep)
     {
-        
+        int rows = arr.GetLength(0);
+        int cols = arr.GetLength(1);
 
-        
+        int[,] newOctopus = new int[rows, cols];
 
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                Tuple<int, int> current = new Tuple<int, int>(row, col);
+                if (flashedThisStep.Contains(current))
+                {
+                    //Console.WriteLine("Holy crap, this one flashed!");
+                    newOctopus[row, col] = 0;
+                }
+                else
+                {
+                    newOctopus[row, col] = arr[row, col];
+                }
+            }
+        }
+        return newOctopus;
+    }
+
+    static int[,] increaseEnergy(int[,] arr)
+    {
+        int rows = arr.GetLength(0);
+        int cols = arr.GetLength(1);
+
+        int[,] newOctopus = new int[rows, cols];
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                newOctopus[row, col] = arr[row, col] + 1;
+            }
+
+        }
+        //printBoard(newOctopus);
+        return newOctopus;
+    }
+
+    static List<Tuple<int, int>> detectFlashed(int[,] arr)
+    {
+        int rows = arr.GetLength(0);
+        int cols = arr.GetLength(1);
+
+        int[,] newOctopus = new int[rows, cols];
+        var flashedList = new List<Tuple<int, int>>();
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                if (arr[row, col] > 9)
+                {
+                    // Flash!
+                    flashedList.Add(new Tuple<int, int>(row, col));
+                }
+            }
+        }
+        //System.Console.WriteLine("Flashed count: " + flashedList.Count);
+        return flashedList;
+
+    }
+
+    static List<Tuple<int, int>> findAdjacent(int[,] arr, Tuple<int, int> coord)
+    {
+        int rows = arr.GetLength(0);
+        int cols = arr.GetLength(1);
+
+        int row = coord.Item1;
+        int col = coord.Item2;
+
+        var adjacentList = new List<Tuple<int, int>>();
+        if (row > 0)
+            adjacentList.Add(new Tuple<int, int>(row - 1, col));
+        if (col > 0)
+            adjacentList.Add(new Tuple<int, int>(row, col - 1));
+        if (row > 0 && col > 0)
+            adjacentList.Add(new Tuple<int, int>(row - 1, col - 1));
+        if (row < rows - 1)
+            adjacentList.Add(new Tuple<int, int>(row + 1, col));
+        if (col < cols - 1)
+            adjacentList.Add(new Tuple<int, int>(row, col + 1));
+        if (row < rows - 1 && col < cols - 1)
+            adjacentList.Add(new Tuple<int, int>(row + 1, col + 1));
+        //System.Console.WriteLine("Length: " + adjacentList.Count);
+        return adjacentList;
+    }
+    static void printBoard(int[,] arr)
+    {
+        int rows = arr.GetLength(0);
+        int cols = arr.GetLength(1);
+        //Console.WriteLine("rows: " + rows);
+        //Console.WriteLine("cols: " + cols);
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                Console.Write(arr[row, col] + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+
+
+    static void ChessBoard()
+    {
         int[] myNumbers = { 14, 22, 7, 19 };
         Console.WriteLine("Welcome to the Chess Board");
         Console.Write("How large board? ----> ");
